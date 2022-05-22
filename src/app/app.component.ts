840f01b2db326916bf00df6b22e8a69040089288
Nivas Donthireddy <nivas.donthireddy@bp.com>
2022-05-22 16:57:26 +0530

    5.  Button Toggle _ Template Ref

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { AfterContentInit, AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { MatButtonToggleGroup } from '@angular/material/button-toggle';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit{
   title = 'material-demo';
+  @ViewChild('toggleGroup') toggleSelection!: MatButtonToggleGroup;
+
+  ngAfterViewInit(): void {
+    this.toggleSelection.valueChange.subscribe(console.log);
+  }
+
+
+
+
+
 }
