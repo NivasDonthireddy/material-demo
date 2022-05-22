46670bd8908be6dce7ce89979b610ab2e455f128
Nivas Donthireddy <nivas.donthireddy@bp.com>
2022-05-22 23:11:01 +0530

    initial commit

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -11,6 +11,8 @@ import {MatListModule} from '@angular/material/list';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatGridListModule} from '@angular/material/grid-list';
 import {MatExpansionModule} from '@angular/material/expansion';
+import {MatCardModule} from '@angular/material/card';
+
 
 const material = [
   MatButtonModule,
@@ -24,7 +26,8 @@ const material = [
   MatListModule,
   MatDividerModule,
   MatGridListModule,
-  MatExpansionModule
+  MatExpansionModule,
+  MatCardModule
 ];
 
 @NgModule({
